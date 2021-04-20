da92c6ba9dbc3561237ac294995f30130537d479
Higor Matheus <hmoliveira83@gmail.com>
2021-04-19 21:15:10 -0300

    add buton signIn

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,17 +1,21 @@
-import React from 'react';
+import React from "react";
+import { SignInButton } from "../SignInButton";
 
-import styles from './styles.module.scss';
+import styles from "./styles.module.scss";
 
 export function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.HeaderContent}>
-        <img src="/images/logo.svg" alt="ig.news"/>
+        <img src="/images/logo.svg" alt="ig.news" />
         <nav>
-          <a href="" className={styles.active}>Home</a>
+          <a href="" className={styles.active}>
+            Home
+          </a>
           <a href="">Posts</a>
         </nav>
+        <SignInButton />
       </div>
     </header>
-  )
+  );
 }
