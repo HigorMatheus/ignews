710c5d12dc42f904b6e1054dc821807a9efffd40
Higor Matheus <hmoliveira83@gmail.com>
2021-04-21 21:21:01 -0300

    add stripe comconsumo a api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,16 @@
+import { GetServerSideProps } from 'next';
 import Head from 'next/head';
+import { SubscribeButton } from '../Components/SubscribeButton';
+import { stripe } from '../services/stripe';
 
 import styles from './home.module.scss';
-export default function Home() {
+interface IHomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  };
+}
+export default function Home({ product }: IHomeProps) {
   return (
     <>
       <Head>
@@ -15,11 +24,30 @@ export default function Home() {
           </h1>
           <p>
             get access to all the publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
+          <SubscribeButton priceId={product.priceId} />
         </section>
         <img src="./images/avatar.svg" alt="Girl coding" />
       </main>
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve('price_1IiothCU9rgvp0PBCmS0Tun4', {
+    expand: ['product'],
+  });
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100),
+  };
+  return {
+    props: {
+      product,
+    },
+  };
+};
